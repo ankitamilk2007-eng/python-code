183316b70eea15f5cd2fabc127f485d5113b80be
ankitamilk2007-eng <ankitamilk2007@gmail.com>
2025-11-17 22:04:57 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,13 @@
+#taking user input
+num = int(input("Enter a number"))
+if num % 2 == 0:
+    print("It is even")
+else:
+    print("It is odd")
+
+#without user input
+a = 8
+if a % 2 == 0:
+    print("even")
+else:
+    print("odd")

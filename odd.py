3d141bf7a6c5e5ace0e342247a75c19284527b22
ankitamilk2007-eng <ankitamilk2007@gmail.com>
2025-11-18 08:01:36 +0530

    Add files via upload

---
+++ odd.py
@@ -0,0 +1,13 @@
+#without input user
+a = 9
+if a % 2 != 0:
+    print("odd")
+else:
+    print("even")
+
+#user input
+num = int(input("enter a number"))
+if num % 2 != 0:
+    print("odd")
+else:
+    print("even")
